7e346fa075a358b8a78d4688ab6ba126e2114e10
nkturtas@gmx.com <nkturtas@gmx.com>
2024-08-20 11:17:03 +0300

    Add project files.

---
+++ CoinFlipGame/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CoinFlipGame
+{
+    partial class coinFlipGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(coinFlipGame));
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.flipButton = new System.Windows.Forms.Button();
+            this.headsLabel = new System.Windows.Forms.Label();
+            this.tailsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Image = ((System.Drawing.Image)(resources.GetObject("imageBox.Image")));
+            this.imageBox.Location = new System.Drawing.Point(210, 26);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(406, 372);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // flipButton
+            // 
+            this.flipButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flipButton.Location = new System.Drawing.Point(210, 470);
+            this.flipButton.Name = "flipButton";
+            this.flipButton.Size = new System.Drawing.Size(406, 76);
+            this.flipButton.TabIndex = 1;
+            this.flipButton.Text = "Flip";
+            this.flipButton.UseVisualStyleBackColor = true;
+            this.flipButton.Click += new System.EventHandler(this.flipButton_Click);
+            // 
+            // headsLabel
+            // 
+            this.headsLabel.BackColor = System.Drawing.Color.White;
+            this.headsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headsLabel.Location = new System.Drawing.Point(215, 415);
+            this.headsLabel.Name = "headsLabel";
+            this.headsLabel.Size = new System.Drawing.Size(207, 40);
+            this.headsLabel.TabIndex = 2;
+            this.headsLabel.Text = "Heads:";
+            this.headsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tailsLabel
+            // 
+            this.tailsLabel.BackColor = System.Drawing.Color.White;
+            this.tailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tailsLabel.Location = new System.Drawing.Point(428, 415);
+            this.tailsLabel.Name = "tailsLabel";
+            this.tailsLabel.Size = new System.Drawing.Size(188, 40);
+            this.tailsLabel.TabIndex = 3;
+            this.tailsLabel.Text = "Tails:";
+            this.tailsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // coinFlipGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(825, 569);
+            this.Controls.Add(this.tailsLabel);
+            this.Controls.Add(this.headsLabel);
+            this.Controls.Add(this.flipButton);
+            this.Controls.Add(this.imageBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "coinFlipGame";
+            this.Text = "Coin Flip Game";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button flipButton;
+        private System.Windows.Forms.Label headsLabel;
+        private System.Windows.Forms.Label tailsLabel;
+    }
+}
+
---
+++ CoinFlipGame/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinFlipGame
+{
+    public partial class coinFlipGame : Form
+    {
+        int heads = 0;
+        int tails = 0;
+        public coinFlipGame()
+        {
+            InitializeComponent();
+        }
+
+        private void flipButton_Click(object sender, EventArgs e)
+        {
+            Random coinFlip = new Random();
+            int result = coinFlip.Next(1, 3);
+
+            if(result == 1)
+            {
+                imageBox.ImageLocation = @"D:\C# Projects\Windows Forms Projects\11 Coin Flip Game\1.png";
+                imageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                heads++;
+                headsLabel.Text = "Heads: " + heads.ToString();
+            }
+            else
+            {
+                imageBox.ImageLocation = @"D:\C# Projects\Windows Forms Projects\11 Coin Flip Game\2.png";
+                imageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                tails++;
+                tailsLabel.Text = "Tails: " + tails.ToString();
+            }
+        }
+    }
+}
